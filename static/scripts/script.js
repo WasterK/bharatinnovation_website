d994d7a4a9779331028e53261b11a10bdbc46cde
Kiran Khaire <khairekiran2@gmail.com>
2023-11-30 00:43:42 +0530

    Home and Product commit 1

---
+++ static/scripts/script.js
@@ -0,0 +1,25 @@
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav-bar").style.top = "0";
+    } else {
+        document.getElementById("nav-bar").style.top = "-60px"; /* Adjust based on the height of your nav bar */
+    }
+
+    prevScrollpos = currentScrollPos;
+};
+
+let navBar = document.getElementById('nav-bar');
+
+navBar.addEventListener('mouseenter', function() {
+    navBar.classList.remove('hidden');
+});
+
+navBar.addEventListener('mouseleave', function() {
+    if (window.scrollY === 0) {
+        navBar.classList.add('hidden');
+    }
+});
